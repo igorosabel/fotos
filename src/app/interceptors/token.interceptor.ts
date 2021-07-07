2e9d9ba45f25b493e9b5f02676c8e5bc21812476
Iñigo Gorosabel Fernández <igorosabel@costa.local>
2021-07-07 14:56:07 +0200

    Empiezo con estructura inicial

---
+++ src/app/interceptors/token.interceptor.ts
@@ -0,0 +1,33 @@
+import {
+  HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { UserService } from '../services/user.service';
+
+@Injectable()
+export class TokenInterceptor implements HttpInterceptor {
+  constructor(public user: UserService) { }
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    request = request.clone({
+      setHeaders: {
+        Authorization: this.user.token ? this.user.token : ''
+      }
+    });
+    return next.handle(request).pipe(
+      catchError((error: HttpErrorResponse) => {
+        let errorMessage = '';
+        if (error.error instanceof ErrorEvent) {
+          // client-side error
+          errorMessage = `Error: ${error.error.message}`;
+        } else {
+          // server-side error
+          errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+        }
+        return throwError(errorMessage);
+      })
+    );
+  }
+}
