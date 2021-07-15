b2fea165f8924306986ab7edb00222cf0ac3089d
Iñigo Gorosabel <inigo.gorosabel@gmail.com>
2021-07-15 08:57:17 +0200

    Empiezo con  add

--- src/app/pages/add/add.component.ts
+++ src/app/pages/add/add.component.ts
@@ -8,6 +8,9 @@ import { UserService } from 'src/app/services/user.service';
 	styleUrls: ['./add.component.scss']
 })
 export class AddComponent implements OnInit {
+	name: string = '';
+	list: String[] = [];
+
 	constructor(private us: UserService, private router: Router) {}
 
 	ngOnInit(): void {
@@ -15,5 +18,31 @@ export class AddComponent implements OnInit {
 		if (!this.us.logged) {
 			this.router.navigate(['/admin']);
 		}
+		else {
+			this.name = this.us.user.name;
+		}
+	}
+
+	addNew(): void {
+		const obj = document.getElementById('add-files');
+		obj && obj.click();
+	}
+
+	onFileChange(event: Event) {
+		const target = (<HTMLInputElement>event.target);
+		if ( target !== null && target.files !== null && target.files.length > 0) {
+			for (let file of target.files) {
+				this.readFile(file);
+			}
+		}
+	}
+
+	readFile(file: File) {
+		let reader = new FileReader();
+		reader.readAsDataURL(file);
+		reader.onload = () => {
+			this.list.push(reader.result as string);
+			(<HTMLInputElement>document.getElementById('add-files')).value = '';
+		};
 	}
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { User } from 'src/app/model/user.model';
-import { DataShareService } from './data-share.service';
 import { ClassMapperService } from './class-mapper.service';
 import { UserInterface } from 'src/app/interfaces/interfaces';
 
@@ -9,13 +8,10 @@ export class UserService {
 	logged: boolean = false;
 	user: User = new User();
 
-	constructor(
-		private dss: DataShareService,
-		private cms: ClassMapperService
-	) {}
+	constructor(private cms: ClassMapperService) {}
 
 	loadLogin(): void {
-		const loginObj = this.dss.getGlobal('login');
+		const loginObj = localStorage.getItem('login');
 		if (loginObj === null) {
 			this.logout();
 		}
@@ -27,12 +23,12 @@ export class UserService {
 	}
 
 	saveLogin(): void {
-		this.dss.setGlobal('login', this.user.toInterFace());
+		localStorage.setItem('login', JSON.stringify(this.user.toInterFace()));
 	}
 
 	logout(): void {
 		this.logged = false;
 		this.user = new User();
-		this.dss.removeGlobal('login');
+		localStorage.removeItem('login');
 	}
 }
