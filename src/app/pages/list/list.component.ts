d5f7a64ed3e77cbf4cfd387ab1a38c16db41abed
Iñigo Gorosabel <inigo.gorosabel@gmail.com>
2021-08-19 18:11:38 +0200

    Filtro de etiquetas terminado

--- src/app/pages/list/list.component.ts
+++ src/app/pages/list/list.component.ts
@@ -12,11 +12,14 @@ import { UserService } from 'src/app/services/user.service';
 	styleUrls: ['./list.component.scss']
 })
 export class ListComponent implements OnInit {
+	sideNavOpened: boolean = false;
 	isAdmin: boolean = false;
 	currentPage: number = 1;
 	numPages: number = 0;
 	list: Photo[] = [];
+	fullList: Photo[] = [];
 	tags: Tag[] = [];
+	selectedTag: Tag = new Tag();
 	showPhoto: boolean = false;
 	selectedPhoto: Photo = new Photo();
 
@@ -37,13 +40,18 @@ export class ListComponent implements OnInit {
 
 			this.as.getPhotos(this.currentPage).subscribe(result => {
 				this.numPages = result.pages;
-				this.list = this.cms.getPhotos(result.list);
+				this.fullList = this.cms.getPhotos(result.list);
+				this.filterPhotos();
 			});
 
 			this.as.getTags().subscribe(result => {
 				this.tags = this.cms.getTags(result.list);
 			});
 		}
+	}
+
+	openSideNav(): void {
+		this.sideNavOpened = true;
 	}
 
 	selectPhoto(photo: Photo): void {
@@ -67,9 +75,34 @@ export class ListComponent implements OnInit {
 		alert(typeof ev);
 	}
 
-	selectTag(tag: Tag, ev: MouseEvent): void {
+	selectTag(tag: Tag | null, ev: MouseEvent): void {
 		ev && ev.preventDefault();
 		console.log(tag);
+		if (tag !== null) {
+			this.selectedTag = tag;
+		}
+		else {
+			this.selectedTag = new Tag();
+		}
+		this.filterPhotos();
+		this.sideNavOpened = false;
+	}
+
+	filterPhotos(): void {
+		if (this.selectedTag.id == -1) {
+			this.list = this.cms.getPhotos( this.fullList.map(x => x.toInterface()) );
+		}
+		else {
+			this.list = [];
+			for (let p of this.fullList) {
+				for (let t of p.tags) {
+					if (t.id == this.selectedTag.id) {
+						this.list.push( this.cms.getPhoto(p.toInterface()) );
+						break;
+					}
+				}
+			}
+		}
 	}
 
 	logout(): void {
